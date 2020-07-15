6b97583b19a4365ce6928abe61ebd40b5b52f213
Javier Zarate <zarate.javier.jz@gmail.com>
2020-07-14 22:28:50 -0600

    added Home component

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Container } from "semantic-ui-react"
+import { Switch, Route } from "react-router-dom"
+import Navbar from "./components/Navbar"
+import Home from "./components/Home"
+import Register from "./components/Register"
+import Login from "./components/Login"
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Container>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/register' component={Register}/>
+        <Route exact path='/login' component={Login}/>
+        
+      </Container>
+    </>
   );
 }
 
