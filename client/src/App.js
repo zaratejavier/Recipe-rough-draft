65c1eb37af52cfecc09460016e85175e54cd643d
Javier Zarate <zarate.javier.jz@gmail.com>
2020-07-20 22:17:56 -0600

    added Login component

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import { Switch, Route } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import Home from "./components/Home"
 import Register from "./components/Register"
-// import Login from "./components/Login"
+import Login from "./components/Login"
 
 
 
@@ -17,7 +17,7 @@ function App() {
       <Container>
         <Route exact path='/' component={Home} />
         <Route exact path='/register' component={Register}/>
-        {/* <Route exact path='/login' component={Login}/> */}
+        <Route exact path='/login' component={Login}/>
         
       </Container>
     </>
