13a2b72b45c6609cbc5f80054a1666e19b892b2b
Javier Zarate <zarate.javier.jz@gmail.com>
2020-08-11 14:13:35 -0600

    added a default image

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,6 +1,8 @@
 import React from "react"
 import { AuthConsumer } from "../providers/AuthProvider"
-import { Container, Divider } from "semantic-ui-react"
+import { Container, Divider, Grid, Image, Header } from "semantic-ui-react"
+
+const defaultImage = 'https://d30y9cdsu7xlg0.cloudfront.net/png/15724-200.png';
 
 class Profile extends React.Component {
   state = {editing: false, formValues: {name: "", email:""}}
@@ -10,16 +12,27 @@ class Profile extends React.Component {
     this.setState({formValues: {name, email}})
   }
 
+  profileView = () => {
+    const { auth: { user }, } = this.props
+    return (
+      <>
+        <Grid.Column width={4}>
+          <Image src={user.image || defaultImage}/>
+        </Grid.Column>  
+        <Grid.Column width={8}>
+          <Header as='h1'>Name: {user.name}</Header>
+          <Header as='h1'>Email: {user.email}</Header>
+
+        </Grid.Column>
+      </>
+    )
+  }
+
   render() {
-    const { formValues: { name, email } } = this.state;
     return (
       <Container>
         <Divider />
-        <h1>Profile</h1>
-        <h1>name: {name}</h1>
-        <h1>email: {email}</h1>
-
-
+        {this.profileView()}
       </Container>
     )
   }
