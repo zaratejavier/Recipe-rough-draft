361c5303690c2be81cb6e5721d14d0b557594772
Javier Zarate <zarate.javier.jz@gmail.com>
2020-08-11 14:28:07 -0600

    added a toggle edit to the profile page

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { AuthConsumer } from "../providers/AuthProvider"
-import { Container, Divider, Grid, Image, Header } from "semantic-ui-react"
+import { Container, Divider, Grid, Image, Header, GridRow, GridColumn, Button, Form } from "semantic-ui-react"
 
 const defaultImage = 'https://d30y9cdsu7xlg0.cloudfront.net/png/15724-200.png';
 
@@ -12,6 +12,14 @@ class Profile extends React.Component {
     this.setState({formValues: {name, email}})
   }
 
+  handleChange = (e) => {
+    
+  }
+
+  handleSubmit = (e) => [
+
+  ]
+  
   profileView = () => {
     const { auth: { user }, } = this.props
     return (
@@ -28,11 +36,54 @@ class Profile extends React.Component {
     )
   }
 
+
+  editView = () => {
+    const { auth: { user }, } = this.props;
+    const { formValues: { name, email } } = this.state;
+    return (
+      <Form onSubmit={this.handleSubmit}>
+        <Grid.Column width={4}>
+        </Grid.Column>
+        <Grid.Column width={8}>
+          <Form.Input
+            label="Name"
+            name="name"
+            value={name}
+            required
+            onChange={this.handleChange}
+          />
+          <Form.Input
+            label="Email"
+            name="email"
+            value={email}
+            required
+            onChange={this.handleChange}
+          />
+          <Button>Update</Button>
+        </Grid.Column>
+      </Form>
+    )
+  }
+
+  toggleEdit = () => {
+    this.setState({editing: !this.state.editing })
+  }
+
   render() {
+    const {editing} = this.state
     return (
       <Container>
         <Divider />
-        {this.profileView()}
+        <Grid>
+          <Grid.Row>
+            {editing ? this.editView() : this.profileView()}
+            <GridColumn>
+              <Button onClick={this.toggleEdit}>
+                {editing ? "Cancel" : "Edit"}
+              </Button>
+            </GridColumn>
+          </Grid.Row>
+        </Grid>
       </Container>
     )
   }
