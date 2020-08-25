9a77e124f4e552a02162194712a17c5c3baed772
Javier Zarate <zarate.javier.jz@gmail.com>
2020-08-24 18:20:12 -0600

    added routes for the new migration and added the controller for the comments

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   namespace :api do
-    resources :recipes
+    resources :recipes do 
+      resources :comments
+    end
+    
     resources :users, only: :update
   end
 end
