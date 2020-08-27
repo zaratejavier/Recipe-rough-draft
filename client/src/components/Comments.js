9a7feab1aa7432732535fc8296520047773c89de
Javier Zarate <zarate.javier.jz@gmail.com>
2020-08-27 11:58:30 -0600

    added auth provider to see the name of the person who commented.

--- client/src/components/Comments.js
+++ client/src/components/Comments.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import Axios from 'axios'
 import { Container } from 'semantic-ui-react'
+import {AuthConsumer} from "../providers/AuthProvider"
 
-const Comments = ({recipeId}) => {
+const Comments = (props) => {
   const [comments, setComments] = useState([])
 
   useEffect(() => {
-    Axios.get(`/api/recipes/${recipeId}/comments`)
+    Axios.get(`/api/recipes/${props.recipeId}/comments`)
       .then(res => {
         setComments(res.data)
       })
@@ -22,10 +23,19 @@ const Comments = ({recipeId}) => {
   return (
     <div>
       <h2>Recipe Comments</h2>
+      {props.auth.user.name}'s
       {renderComments()}
 
     </div>
   )
 }
 
-export default Comments
+const ConnectedComments = (props) => {
+  return (
+  <AuthConsumer>
+    {auth=> (<Comments {...props} auth={auth} />)}
+  </AuthConsumer>
+  )
+}
+
+export default ConnectedComments
