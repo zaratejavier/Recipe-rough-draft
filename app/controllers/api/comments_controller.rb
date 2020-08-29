c2e571365c5c03859e6a5f3a57fa2172c8b323f5
Javier Zarate <zarate.javier.jz@gmail.com>
2020-08-28 20:07:27 -0600

    backend and front end are now working for editing a comment

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,5 +1,6 @@
 class Api::CommentsController < ApplicationController
   before_action :set_recipe
+  before_action :set_comment, only: [:update, :destroy]
 
   def index
     render json: @recipe.comments
@@ -23,13 +24,23 @@ class Api::CommentsController < ApplicationController
 
   end
 
-  def edit
+  def update
+    if @comment.update(comment_params)
+      render json: @comment
+    else
+      render json: {errors: comment.errors}, status: :unprocessble_entity
+    end
   end
 
   def destroy
   end
 
   private
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
   def comment_params
     params.require(:comment).permit(:body, :recipe_id, :user_id)
   end
